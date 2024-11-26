2cd74adaa5716a012c499c757fb044bace68dbd2
Vicsta <vtreaba@gmail.com>
2024-11-25 23:37:09 -0500

    Testing loading pages fixed div ids on index.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,6 @@
+console.log("in main");
 window.addEventListener("load", function () {
+    console.log("trying to load");
     loadPage("templates/card.html", "cardContainer", "static/js/card.js");
-}
+    console.log(loadPage);
+});
