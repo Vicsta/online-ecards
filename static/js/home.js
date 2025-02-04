1cb3e078fa964760b63f040e490df95f73027b93
WarcraftExiles <vtreaba@gmail.com>
2025-02-04 17:42:12 -0500

    Trying reduced load .js

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,8 @@
 console.log("in home.js");
 // loadPage("templates/card.html", "cardContainer", "static/js/card.js");
 
+console.log("started onloading");
+console.log(replaceCardsWithTemplate);
 window.onload = () => {
     console.log("finished onloading");
     console.log(replaceCardsWithTemplate);
