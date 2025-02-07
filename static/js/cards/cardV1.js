417f26ed3484a16726db00751f0a3064d09a3b5a
WarcraftExiles <vtreaba@gmail.com>
2025-02-06 19:32:27 -0500

    work on create menus

---
+++ static/js/cards/cardV1.js
@@ -0,0 +1,47 @@
+cardExample1 = {
+    version: 1.0,
+
+    // Backgrounds
+    front: "",
+    page1: "",
+    page2: "",
+    back: "",
+
+    s1: "",
+    s2: "",
+    s3: "",
+
+    s4: "",
+    s5: "",
+    s6: "",
+
+    s7: "",
+    s8: "",
+    s9: "",
+
+    s10: "",
+    s11: "",
+    s12: "",
+
+    font: "",
+    fontSize: "",
+    alignItems: "",
+    justifyContent: "",
+    padding: "",
+
+    text1: "",
+    text2: "",
+    text3: "",
+
+    text4: "",
+    text5: "",
+    text6: "",
+
+    text7: "",
+    text8: "",
+    text9: "",
+
+    text10: "",
+    text11: "",
+    text12: "",
+};
