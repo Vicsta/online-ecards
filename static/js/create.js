70cc332f3a17659cfae4fba98af10c6bd761f523
WarcraftExiles <vtreaba@gmail.com>
2025-02-07 00:15:50 -0500

    work on create menus

--- static/js/create.js
+++ static/js/create.js
@@ -10,6 +10,31 @@ function runCreatePage() {
             createCard = card;
         }
     );
+
+    const tabs = document.querySelectorAll(".menuPageTab");
+    const menus = document.querySelectorAll(".leftMenu");
+
+    // Function to switch active menu
+    function switchMenu(index) {
+        // Hide all menus
+        menus.forEach(menu => menu.classList.add("hidden"));
+        // Show the selected menu
+        menus[index].classList.remove("hidden");
+
+        // Remove selection from all tabs
+        tabs.forEach(tab => tab.classList.remove("menuPageTabSelected"));
+        // Highlight the active tab
+        tabs[index].classList.add("menuPageTabSelected");
+    }
+
+    // Attach click event listeners to all tabs
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => switchMenu(index));
+    });
+
+    // Initially set the first tab as selected
+    tabs[0].classList.add("menuPageTabSelected");
+
 }
 
 // Ensure this runs if the script is executed after DOM is ready
