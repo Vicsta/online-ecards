1f0859e472ee9100eb139c41627a686d0db80b96
Victor Treaba <vtreaba@gmail.com>
2026-05-04 23:16:07 -0400

    remove obnoxious ads

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10965193
+    "zoneId": 10964926
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
