d3fe6ab9a88b6364c933b4b7b229088ae161f53b
Victor Treaba <vtreaba@gmail.com>
2026-05-04 23:10:56 -0400

    test multi ad

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10964926
+    "zoneId": 10965193
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
